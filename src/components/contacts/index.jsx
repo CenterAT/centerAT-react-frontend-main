76139b40da45c8c49053c6e677e59767b8b995b4
Ruslan Rzaev <69870105+l9llika@users.noreply.github.com>
2024-08-13 17:25:31 +0700

    feat: add map, add swiper
    map: ready for use
    swiper: slider in progress

--- src/components/contacts/index.jsx
+++ src/components/contacts/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { ContactsWrap } from '../contacts-wrapper'
+import { MapWrapper } from '../map'
 
 export const Contacts = () => {
   return (
     <div className='px-5 mb-24 m-auto max-w-[1320px] flex gap-8 m:flex-col m:p-0'>
         <div className=' w-[768px] h-[470px] bg-pink-300 sm:w-full'>
-          Аздесь совсем скоро будет карта!
+          <MapWrapper />
         </div>
         <ContactsWrap />
     </div>
